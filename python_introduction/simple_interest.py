afc7ee08df471b74dc7fdf12d82712bf55903646
Promise16-alt <promisenwachinemere33@gmail.com>
2025-02-01 14:48:12 +0100

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,7 @@
 # I = P * R * T
-P = principal = 1000
-R = rate = 0.05
-T = time = 3
-I = interest = P * R * T
-interest = (rate * principal * time)
-print (f" The simple interest is {interest}")
+principal = 1000
+rate = 0.05
+time = 3
+interest = P * R * T
+interest = rate * principal * time
+print (f" The simple interest is: {interest}")
