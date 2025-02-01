d34a11f77a2431d3f98f74e42631d03ed159e973
Promise16-alt <promisenwachinemere33@gmail.com>
2025-02-01 10:55:27 +0100

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,11 +1,10 @@
 number1 = 10
 number2 = 5
-add = 15
-subtract = 5
-multiply = 50
-divide = 2
+add = number1 + number2
+subtract = number1 - number2
+multiply =  number1 * number2
+divide =  number1 / number2
 
 print (f" addition of {number1} and {number2} is {add}\n")
-
 print (f" subtraction of {number1} and {number2} is {subtract}\n")
 print (f" multiplication of {number1} and {number2} is {multiply}\n")
