205bc3aa901a6abb67c1986c1cdde09be8eb12d1
Promise16-alt <promisenwachinemere33@gmail.com>
2025-02-02 08:10:42 +0100

    Create match_case_calculator.py

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,11 @@
+num1 = int((input("Enter the first number:  ")))
+num2 = int((input("Enter the second number:")))
+operation= input("Choose the operation (+, -, *, /: " )
+if operation == "+":
+    print (f" The result is {num1  + num2}")
+elif operation == "-":
+    print (f"The result is {num1 - num2}")
+elif operation == "*":
+    print (f" The result is {num1 * num2}")
+else:
+    print (f" The result is {num1 / num2}")
